ddddd5d87eab829351af616891683b158aa68702
Kolby MacDonald <115326488+Kolby-MacDonald@users.noreply.github.com>
2023-08-13 22:12:34 -0600

    Lot's of comments.
    Mostly Comments
    Some colour changes and small styling changes.

--- app.py
+++ app.py
@@ -4,125 +4,126 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, current_user, login_user, login_required, logout_user
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_container.models import Users  # Import your Users model from the correct path
+from database_container.models import Users
 import hashlib
 import os
 import re
 
-
-load_dotenv() # Load Environment Variables
-
-app = Flask(__name__) # Define the application.
-app.config['SECRET_KEY'] = os.getenv('APP_KEY') # Development Key Environment Variable
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-# Create a SQLAlchemy engine and session
-engine = create_engine(f"sqlite:///{os.getenv('DB_LOC')}", echo=True)
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# Simulating a user database
+# Class to obtain user specific credentials.
 class User(UserMixin):
     def __init__(self, id, role):
         self.id = id
         self.role = role
 
+# Environment variables.
+load_dotenv()
 
-@app.route('/')
-def blog():
+# Instance of flask at this root.
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('APP_KEY')
 
-    return render_template('blog.html')
+# Instance of login manager for managing user sessions.
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+# Create an SQLite Engine and print the queries made (remove echo after debugg).
+engine = create_engine(f"sqlite:///{os.getenv('DB_LOC')}", echo=True)
+
+# Object Relational Mapping database cursor (essentially).
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Load connecting users into user class.
 @login_manager.user_loader
 def load_user(id):
     user = session.query(Users).get(id)
-    print(user)
     if user:
-        return User(id=id, role=user.role)
-    return None
+        return User(id=id, role=user.role) # If user is controlled, make an instance.
+    return None # Gaurentee's no false authenitication.
 
-def is_safe_path(basedir, path):
-    return os.path.realpath(path).startswith(basedir)
-
+# Use form to login users.
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
+    if request.method == 'POST':  # If "submit" is given:
+        username = request.form['username'] # Get form credentials.
         input_password = request.form['password']
-        hashed_input_password = hashlib.sha3_512(input_password.encode()).hexdigest()  # Hash the input password
+        hashed_input_password = hashlib.sha3_512(input_password.encode()).hexdigest()  # Hash SHA#-512
 
-        user = session.query(Users).filter_by(username=username).first()
+        user = session.query(Users).filter_by(username=username).first() # Usernames are unique, grab first intance.
 
-        if user and user.password == hashed_input_password:  # Compare hashed passwords
-            user_obj = User(user.id, user.role)  # Create a User object for Flask-Login with id and role
-            login_user(user_obj)
-            return redirect(url_for('home'))
+        if user and user.password == hashed_input_password: # If user exist's and the password matches stored password:
+            user_obj = User(user.id, user.role) # Create the user object.
+            login_user(user_obj) # Flask session for that user object.
+            return redirect(url_for('blog')) # Return To Blog
         else:
-            flash('Invalid credentials. Please try again.', 'error')
+            flash('Invalid credentials. Please try again.', 'error') # Error message for credentials.
     return render_template('login.html')
 
+# Use form to sign users up.
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         username = request.form['username']
+        email = request.form['email']
         password1 = request.form['password1']
         password2 = request.form['password1']
-        email = request.form['email']
         
-        if len(username) <= 5:
+        # Order failed states for invalid attempts.
+        if len(username) <= 5:                                                                           # Username must be 6 characters minimum
             flash('Username Must Be 6 Characters Minimum.', 'error')
-        elif session.query(session.query(Users).filter_by(username=username).exists()).scalar() == True:
+        elif session.query(session.query(Users).filter_by(username=username).exists()).scalar() == True: # Username must not be taken.
             flash('Username Taken.', 'error')
-        elif session.query(session.query(Users).filter_by(email=email).exists()).scalar() == True:
+        elif session.query(session.query(Users).filter_by(email=email).exists()).scalar() == True:       # Email must not be taken.
             flash('Email Taken.', 'error')
-        elif re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email) is None:
+        elif re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email) is None:               # REGEX email to control it's format.
             flash('Email Has Invalid Format.', 'error')
-        elif password1 != password2:
+        elif password1 != password2:                                                                     # Password and Confirm Password must match.
             flash('Password\'s Don\'t Match.', 'error')
-        elif len(password1) <= 7:
+        elif len(password1) <= 7:                                                                        # Password must be 8 characters minimum.
             flash(f'Password Must Be 8 Characters Minimum.', 'error')
-        elif not any(char.islower() for char in password1):
+        elif not any(char.islower() for char in password1):                                              # Password must contain 1 lowercase.
             flash(f'Password Must Have 1 Lowercase Letter.', 'error')
-        elif not any(char.isupper() for char in password1):
+        elif not any(char.isupper() for char in password1):                                              # Password must contain 1 uppercase.
             flash(f'Password Must Have 1 Uppercase Letter.', 'error')
-        elif not any(char in '!@#$%^&*()_+[]{}|;:,.<>?/~`"\'\\' for char in password1):
+        elif not any(char in '!@#$%^&*()_+[]{}|;:,.<>?/~`"\'\\' for char in password1):                  # Password must contain 1 special character.
             flash(f'Password Must Have 1 Special Character.', 'error')
         else:
             try:
-                role = "CompanyUser" # Just for now
-                hashed_password = hashlib.sha3_512(password1.encode()).hexdigest()
-                new_user = Users(username=username, password=hashed_password, role=role, email=email, company="Demo")
-                session.add(new_user)
-                session.commit()
-                flash('Account created successfully!', 'success')
-                return redirect(url_for('login'))
+                # INCLUDE SQLI CONTROLLER
+                role = "User" # Create a new user (later 2FAC will change this state).
+                hashed_password = hashlib.sha3_512(password1.encode()).hexdigest() # SHA3-512 Hash their password.
+                new_user = Users(username=username, password=hashed_password, role=role, email=email, company="Demo") # Create a user instance.
+                session.add(new_user) # Add a new user to the database.
+                session.commit() # Save that user to the database.
+                flash('Account created successfully!', 'success') # Success message.
+                return redirect(url_for('login')) # Redirect to login.
             except:
                 flash(f'Error: If persist\'s, contact X@gmail.com.', 'error')
 
     return render_template('signup.html')
 
+# Initial Page Upon Accessing the website
+@app.route('/')
+def blog():
+    user_status = "logged_out" # Assume the user logged out.
+    if current_user.is_authenticated: # Check if they are logged in.
+        user_status = "logged_in" #If so set the logged in status to logged in.
+        if current_user.role.lower() == "admin": # If the user is an admin (we're going to give them extra functionality).
+            pass
+        
+        elif current_user.role.lower() == "user": # If they are logged in they will get some extra functionality as well.
+            pass
+    
+    return render_template('blog.html', user_status=user_status)
 
-@app.route('/admin', methods=['GET'])
+# A page only accessable to admins.
+@app.route('/admin')
 @login_required
-def admin():
+def admin_page():
     if current_user.role.lower() != "admin":
-        # Redirect non-admin users to a restricted page or show an error message.
-        return redirect(url_for('home'))
-    # Render the admin page for admin users.
+        return redirect(url_for('blog'))
+
     return render_template('admin.html')
-
-@app.route('/home')
-@login_required
-def home():
-    is_admin = current_user.role.lower() == "admin"  # Check if user is admin
-    return render_template('home.html', is_admin=is_admin)
-
-@app.route('/restricted')
-@login_required
-def restricted():
-    return render_template('restricted.html')
 
 @app.route('/logout')
 @login_required
@@ -130,12 +131,14 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.errorhandler(401)
-@app.errorhandler(403)
-@app.errorhandler(404)
-@app.errorhandler(500)
+# Error control for directory traversal attack.
+# Hidding web status codes related to traversal prevents page discovery.
+@app.errorhandler(401) # Unauthorized Access ( Not logged in )
+@app.errorhandler(403) # Forbidden Access ( Logged in )
+@app.errorhandler(404) # Resource Not Found ( Page Doesn't Exist )
+@app.errorhandler(500) # Server Error ( Provides log details about the website. )
 def error_handler(e):
-    return redirect(url_for('login'))
+    return redirect(url_for('blog'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database_container/models.py
+++ database_container/models.py
@@ -1,31 +1,55 @@
-from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy import create_engine, Column, String, Integer, LargeBinary
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from flask_login import UserMixin
 from dotenv import load_dotenv
-import os
-import hashlib
-
-load_dotenv()
 
 Base = declarative_base()
 
+# Website Users
 class Users(Base, UserMixin):
     __tablename__ = "users"
 
-    id = Column("id", Integer, primary_key=True, autoincrement=True)
-    username = Column("firstName", String, unique=True)
-    password = Column("password", String)
-    role = Column("role", String)  # Admin, CompanyOwner, CompanyUser, Banned
-    email = Column("email", String, unique=True)
-    company = Column("company", String)
+    id = Column("id", Integer, primary_key=True, autoincrement=True)            # User ID
+    username = Column("firstName", String, unique=True)                         # Unique Usernames
+    password = Column("password", String)                                       # Password (SHA3_512) - Will be salted
+    role = Column("role", String)                                               # User Role: Admin, User, Banned
+    email = Column("email", String, unique=True)                                # Unique Email - Two Faq Eventually
 
     def __init__(self, username, password, role, email, company):
         self.username = username
         self.password = password
         self.role = role
         self.email = email
-        self.company = company
 
     def __repr__(self):
-        return f"{self.id} | {self.username} | {self.password} | {self.role} | {self.email} | {self.company}"
+        return f"{self.id} | {self.username} | {self.password} | {self.role} | {self.email}"
+
+# Blog Post's 
+class BlogPosts(Base):
+    __tablename__ = "blog_posts"
+    
+    id = Column("id", Integer, primary_key=True, autoincrement=True)            # Post ID
+    title = Column("title", String)                                             # Post Title
+    content = Column("content", String)                                         # Post Body Content
+    header_image = Column("header_image", LargeBinary)                          # Post Header Image
+
+    def __init__(self, title, content, header_image):
+        self.title = title
+        self.content = content
+        self.header_image = header_image
+
+# Remote Access Database Controller [ AVAILDATA ] - Contact developer for more information.
+class AvailAccess(Base):
+    __tablename__ = "a000_users_table"                                          # Standard Access Table Name
+
+    id = Column(Integer, primary_key=True, autoincrement=True)                  # Test Account ID
+    username = Column(String, unique=True)                                      # Unique Username
+    password = Column(String)                                                   # SHA256 Password
+    role = Column(String, default="signed_up")                                  # User Role: Admin, User
+    write_access = Column(String)                                               # What tables can you write to.
+    read_access = Column(String)                                                # What tables can you read.
+    create_tables = Column(String, default="no")                                # Can you create tables.
+    employee_name = Column(String)                                              # Required Column - Don't use.
+    job_title = Column(String)                                                  # Required Column - Don't use.
+    email = Column(String, unique=True)                                         # Unique Email
